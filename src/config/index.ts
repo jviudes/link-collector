a4aa04943822b9159d0378cf093276e3906a8b52
Jhonathan Viudes <jhonathan.viudes@gmail.com>
2020-08-09 19:13:43 -0300

    changed db init to MongoLink

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
-import Mongo from '../dbs/Mongo';
+import MongoLink from '../dbs/MongoLink';
 
 const dbAdress = process.env.MONGO_ADDRESS_LINKS || '';
-const dbLinks = new Mongo(dbAdress);
+const dbLinks = new MongoLink(dbAdress);
 
 export { dbLinks };
